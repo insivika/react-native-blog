24f3e9bb74520e90eb9326413b1cee83bbd73481
insivika <phil@insivika.com>
2020-06-16 21:38:18 -0700

    first commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+const IndexScreen = () => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: "grey",
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
+
+export default IndexScreen;
